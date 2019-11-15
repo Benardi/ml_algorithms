e216359723f718d0bf0d10756de7fd1db28ee55a
Benardi <benardinunes@gmail.com>
2019-11-15 08:32:11 -0300

    Add unit tests for lgx_rg/cmpt_grf

--- touvlo/supv/lgx_rg/cmpt_grf.py
+++ touvlo/supv/lgx_rg/cmpt_grf.py
@@ -1,4 +1,4 @@
-from numpy import log, zeros, dot, squeeze
+from numpy import log, zeros, dot
 from numpy import sum as add
 
 from touvlo.utils import g
@@ -8,38 +8,39 @@ def h(X, w, b):
     return g(dot(w.T, X) + b)
 
 
-def cost_func(X, Y, h):
-    m = len(Y)
-    J = -(1 / m) * add((Y * log(h) + (1 - Y) * log(1 - h)))
+def cost_func(X, Y, hyp=None, **kwargs):
+    if hyp is None:
+        hyp = h(X, **kwargs)
+
+    _, m = Y.shape
+    J = -(1 / m) * add((Y * log(hyp) + (1 - Y) * log(1 - hyp)))
 
     return J
 
 
-def grad(w, b, X, Y):
+def grad(X, Y, w, b):
 
-    m, _ = X.shape
+    _, m = X.shape
     A = h(X, w, b)  # compute activation
-    cost = cost_func(X, Y, A)
 
     dz = A - Y
     dw = (1 / m) * dot(X, dz.T)
     db = (1 / m) * add(dz)
-    cost = squeeze(cost)
 
     return dw, db
 
 
-def predict(w, b, X):
-    m, _ = X.shape
-    Y_prediction = zeros((1, m))
-    w = w.reshape(m, 1)
+def predict(X, w, b):
+    n, m = X.shape
+    Y_hat = zeros((1, m))
+    w = w.reshape(n, 1)
 
     A = h(X, w, b)
 
     for i in range(A.shape[1]):
         if A[:, i] >= 0.5:
-            Y_prediction[:, i] = 1
+            Y_hat[:, i] = 1
         else:
-            Y_prediction[:, i] = 0
+            Y_hat[:, i] = 0
 
-    return Y_prediction
+    return Y_hat
